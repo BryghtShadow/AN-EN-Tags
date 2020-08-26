204f94564c6aa16d9491becb769bc170629a68eb
BryghtShadow <bryghtshadow@gmail.com>
2020-08-27 03:36:00 +1200

    wip

--- js/mission.js
+++ js/mission.js
@@ -1,13 +1,10 @@
-function pad(n) {
-	return n < 10 ? '0' + n : n;
-}
 var app = new Vue({
 	el: '#app',
 	data: {
 		selectedDate: new Date(),
 		period: (new Date(Date.now() - 11*60*60*1000)).getUTCDay() || 7,
-		missions: {},
-		items: {},
+		missionTable: {},
+		itemTable: {},
 		mode: 'single',
 		formats: {
 			input: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
@@ -16,61 +13,61 @@ var app = new Vue({
 	},	
 	created() {
 		fetch('json/gamedata/en_US/gamedata/excel/mission_table.json').then(r=>r.json()).then(data=>{
-			this.missions = data
+			this.missionTable = data
 		})
 		fetch('json/gamedata/en_US/gamedata/excel/item_table.json').then(r=>r.json()).then(data=>{
-			this.items = data.items
+			this.itemTable = data.items
 		})
 	},
 	computed: {
 		dailies() {
-			if (!this.missions.dailyMissionPeriodInfo) return {};
-			let found = this.missions.dailyMissionPeriodInfo.find(e=>{
-				return (
-					e.startTime * 1000 <= this.selectedDate &&
-					this.selectedDate <= e.endTime * 1000
-				)
-			})
-			return found
-		},
-		dailyPeriod() {
-			let dailies = this.dailies
-			if (!dailies.periodList) return null;
-			return dailies.periodList.find(e=>{
-				return e.period.includes(this.period)
-			})
+			if (!this.missionTable.dailyMissionPeriodInfo) return {};
+			let info = this.missionTable.dailyMissionPeriodInfo.find(e=>{
+				return e.startTime * 1000 <= this.selectedDate
+					&& this.selectedDate <= e.endTime * 1000;
+			});
+			if (!info) return {};
+			info.periodList = (info.periodList || []).filter(e=>{
+				return e.period.includes(this.period);
+			});
+			return info;
 		},
 		dailyTasks() {
-			let periodData = this.dailyPeriod;
-			if (!periodData) return [];
-			let missionIds = this.missions.missionGroups[periodData.missionGroupId].missionIds
-			let results = missionIds.map((missionId, missionIndex)=>{
-				return this.missions.missions[missionId];
-			})
-			return results
+			if (!this.dailies.periodList) return [];
+			return this.dailies.periodList.map(period=>{
+				let k = period.missionGroupId
+				let ids = this.missionTable.missionGroups[k].missionIds
+				return ids.map(id => this.missionTable.missions[id])
+			});
 		},
 		dailyRewards() {
-			let periodData = this.dailyPeriod;
-			if (!periodData) return [];
-			let periodicalRewards = this.missions.periodicalRewards
-			if (!this.missions.periodicalRewards) return [];
-			let results = Object.values(periodicalRewards).filter(reward=>{
-				return reward.groupId === periodData.rewardGroupId
-			})
-			return results
-		}
+			if (!this.dailies.periodList) return [];
+			return this.dailies.periodList.map(period=>{
+				let k = period.rewardGroupId
+				let rewards = Object.values(this.missionTable.periodicalRewards)
+				return rewards.filter(r => r.groupId === k)
+			});
+		},
 	},
 	methods: {
+		getItemName(item) {
+			if (!this.itemTable[item.id]) {
+				return '???';
+			}
+			return this.itemTable[item.id].name
+		},
 		getItemIcon(item) {
-			let itemData = this.items[item.id]
-			if (!itemData) return 'img/chara/empty.png'
-			return `img/items/${itemData.iconId}.png`
+			if (!this.itemTable[item.id]) {
+				return 'img/chara/empty.png';
+			}
+			return `img/items/${this.itemTable[item.id].iconId}.png`;
 		},
 	},
 	filters: {
 		timestamp: function(d) {
 			if (!d) return "(n/a)";
 			d = new Date(d);
+			const pad = (n) => n < 10 ? '0' + n : n;
 			return (
 				d.getFullYear() + '/' +
 				pad(d.getMonth() + 1) + '/' +
@@ -79,6 +76,7 @@ var app = new Vue({
 				pad(d.getMinutes()) + ':' +
 				pad(d.getSeconds())
 			);
-		}
+		},
+
 	}
 })
