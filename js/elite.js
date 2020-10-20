d0798de9eb0f37979bdbe1a9c5ebb9b7502cd317
BryghtShadow <bryghtshadow@gmail.com>
2020-10-20 18:34:54 +1200

    :lappdumb:

--- js/elite.js
+++ js/elite.js
@@ -30,28 +30,45 @@ Promise.all(promises).then(args=>{
 
 	Object.entries(DB.character_table)
 	.forEach(([k, v]) => {
+		if (v.profession === 'TRAP' || v.profession === 'TOKEN') {
+			return;
+		}
 		if (v.name == 'Lappland') {
 			console.log(k, v)
+
+			var h2 = document.createElement('h3')
+			h2.textContent = v.name
+			body.appendChild(h2)
+
+			v.allSkillLvlup.forEach((skillLvlup, idx) => {
+				let hr = document.createElement('hr')
+				let pre = document.createElement("pre")
+				pre.textContent = `SLvl.${idx+1} -> SLvl.${idx+2}:\n`
+				+ JSON.stringify(skillLvlup, null, '  ')
+				// body.appendChild(pre)
+				// body.appendChild(hr)
+			})
+
 			v.skills.forEach((skill, skillIdx) => {
 				let hr = document.createElement('hr')
 				let pre = document.createElement("pre")
 				pre.textContent = `S${skillIdx}:\n`
 				+ JSON.stringify(skill, null, '  ')
 				+ JSON.stringify(DB.skill_table[skill.skillId], null, '  ')
-				body.appendChild(pre)
-				body.appendChild(hr)
+				// body.appendChild(pre)
+				// body.appendChild(hr)
 			})
 
 			v.phases.forEach((phase, phaseIdx) => {
 				let hr = document.createElement('hr')
 				let pre = document.createElement("pre")
 				let evolveCost = phase.evolveCost
-				if (evolveCost != null) {
+				// if (evolveCost != null) {
 					pre.textContent = `E${phaseIdx} - evolve cost:\n`
 					+ JSON.stringify(evolveCost, null, '  ')
-					body.appendChild(pre)
-					body.appendChild(hr)
-				}
+					// body.appendChild(pre)
+					// body.appendChild(hr)
+				// }
 			})
 		}
 	})
