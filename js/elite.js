9283890769876a4d09207b9f65ed618943a2548b
BryghtShadow <bryghtshadow@gmail.com>
2020-10-21 18:02:42 +1200

    :lappkek:

--- js/elite.js
+++ js/elite.js
@@ -44,32 +44,45 @@ Promise.all(promises).then(args=>{
 			v.allSkillLvlup.forEach((skillLvlup, idx) => {
 				let hr = document.createElement('hr')
 				let pre = document.createElement("pre")
-				pre.textContent = `SLvl.${idx+1} -> SLvl.${idx+2}:\n`
-				+ JSON.stringify(skillLvlup, null, '  ')
-				// body.appendChild(pre)
-				// body.appendChild(hr)
+				let txt = `SLv.${idx+1} -> SLv.${idx+2}:`
+				+ '\n* E'+skillLvlup.unlockCond.phase + "Lv." + skillLvlup.unlockCond.level
+				+ skillLvlup.lvlUpCost.map(item=>{
+					return `\n* [${DB.item_table.items[item.id].name}]x${item.count}`
+				}).join('')
+				pre.textContent = txt
+				body.appendChild(pre)
+				body.appendChild(hr)
 			})
 
 			v.skills.forEach((skill, skillIdx) => {
 				let hr = document.createElement('hr')
 				let pre = document.createElement("pre")
-				pre.textContent = `S${skillIdx}:\n`
-				+ JSON.stringify(skill, null, '  ')
-				+ JSON.stringify(DB.skill_table[skill.skillId], null, '  ')
-				// body.appendChild(pre)
-				// body.appendChild(hr)
+				let txt = `S${skillIdx+1}:`
+				skill.levelUpCostCond.forEach((x, masteryIndex)=>{
+					let lv = masteryIndex+1
+					txt += `\nM${lv} -> M${lv+1}:`
+					txt += x.levelUpCost.map(item=>{
+						return `\n* [${DB.item_table.items[item.id].name}]x${item.count}`
+					})
+				})
+				pre.textContent = txt
+				body.appendChild(pre)
+				body.appendChild(hr)
 			})
 
 			v.phases.forEach((phase, phaseIdx) => {
 				let hr = document.createElement('hr')
 				let pre = document.createElement("pre")
 				let evolveCost = phase.evolveCost
-				// if (evolveCost != null) {
-					pre.textContent = `E${phaseIdx} - evolve cost:\n`
-					+ JSON.stringify(evolveCost, null, '  ')
-					// body.appendChild(pre)
-					// body.appendChild(hr)
-				// }
+				if (evolveCost != null) {
+					let txt = `E${phaseIdx} - evolve cost:`
+					txt += evolveCost.map(item => {
+						return `\n* [${DB.item_table.items[item.id].name}]x${item.count}`						
+					})
+					pre.textContent = txt
+					body.appendChild(pre)
+					body.appendChild(hr)
+				}
 			})
 		}
 	})
